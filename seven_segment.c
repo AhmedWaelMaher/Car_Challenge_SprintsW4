c69ad245cbcaa19c5a35312232a673ccea26e158
AhmedWael <ahmed.wael.maher95@gmail.com>
2019-09-25 19:36:34 +0200

    dummy_app_2 (Seven_Segment)

---
+++ seven_segment.c
@@ -0,0 +1,78 @@
+/******************************************************************************
+ *
+ * Module: Seven Segment
+ *
+ * File Name: seven_segment.c
+ *
+ * Description: Source file for Seven Segment
+ *
+ * Author: Ahmed Wael
+ *
+ *******************************************************************************/
+
+#include "seven_segment.h"
+
+void displayDigit(uint8 digit){
+	switch(digit){
+	case 0 :
+		Dio_write(PORT_A,PIN0,LOW);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 1 :
+		Dio_write(PORT_A,PIN0,HIGH);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 2 :
+		Dio_write(PORT_A,PIN0,LOW);
+		Dio_write(PORT_A,PIN1,HIGH);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 3 :
+		Dio_write(PORT_A,PIN0,HIGH);
+		Dio_write(PORT_A,PIN1,HIGH);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 4 :
+		Dio_write(PORT_A,PIN0,LOW);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,HIGH);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 5 :
+		Dio_write(PORT_A,PIN0,HIGH);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,HIGH);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 6 :
+		Dio_write(PORT_A,PIN0,LOW);
+		Dio_write(PORT_A,PIN1,HIGH);
+		Dio_write(PORT_A,PIN2,HIGH);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 7 :
+		Dio_write(PORT_A,PIN0,HIGH);
+		Dio_write(PORT_A,PIN1,HIGH);
+		Dio_write(PORT_A,PIN2,HIGH);
+		Dio_write(PORT_A,PIN3,LOW);
+		break;
+	case 8 :
+		Dio_write(PORT_A,PIN0,LOW);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,HIGH);
+		break;
+	case 9 :
+		Dio_write(PORT_A,PIN0,HIGH);
+		Dio_write(PORT_A,PIN1,LOW);
+		Dio_write(PORT_A,PIN2,LOW);
+		Dio_write(PORT_A,PIN3,HIGH);
+		break;
+	}
+}
---
+++ seven_segment.h
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *
+ * Module: Seven Segment
+ *
+ * File Name: seven_segment.c
+ *
+ * Description: Header file for Seven Segment
+ *
+ * Author: Ahmed Wael
+ *
+ *******************************************************************************/
+
+#ifndef SEVEN_SEGMENT_H_
+#define SEVEN_SEGMENT_H_
+
+#include "dio.h"
+
+/*
+ * Function Prototypes for Seven Segment
+ */
+void displayDigit(uint8 digit);
+
+#endif /* SEVEN_SEGMENT_H_ */
